4d421be4d52b9fa8acfb6d298fa9908c04735344
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-21 22:04:07 -0700

    add initial nav structure without links, just text

---
+++ src/Nav.jsx
@@ -0,0 +1,23 @@
+function Nav() {
+  return (
+    <nav>
+      <div>
+        Home
+      </div>
+      <div>
+        About
+      </div>
+      <div>
+        Services
+      </div>
+      <div>
+        Portfolio
+      </div>
+      <div>
+        Contact
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
