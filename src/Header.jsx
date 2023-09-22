663054895f51f00b2e72ee174ce2f88d2aed7d52
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-21 22:09:14 -0700

    add logo image and nav import

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import Nav from './Nav.jsx'
+
+function Header() {
+  return (
+    <header>
+      <img src='./assets/Polaris-black.jpg' alt='Polaris Logo'/>
+      <Nav />
+    </header>
+  )
+}
+
+export default Header
