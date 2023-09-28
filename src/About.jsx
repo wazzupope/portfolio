602be761700edb53595052cc09bad7f2b9531176
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-27 22:53:05 -0700

    add initial copy and structure

---
+++ src/About.jsx
@@ -0,0 +1,22 @@
+function About() {
+  return (
+    <section>
+      <div>
+        <h2>About</h2>
+        <p>Founded by a physical therapist, Polaris is the authority on fast websites for medical clinics, wellness practitioners, and sporting organizations. Here&apos;s what sets Polaris apart:</p>
+      </div>
+      <div>
+        <h3>Communication</h3>
+        <p>You will never be left in the dark about the status of your project. Reach out via your preferred method, hear back within 24 hours.</p>
+      </div>
+        <h3>Control</h3>
+        <p>Choose how many revisions to your project you would like. Want complete control over design, layout, copy writing, all of it? Don&apos;t want to be bothered until it&apos;s done? Whatever you prefer, you got it.</p>
+      <div>
+        <h3>Conversion</h3>
+        <p>Our sites are optimized for speed and search engine performance. Care is also put into writing the copy for the site to ensure that your target audience gives you the attention you deserve.</p>
+      </div>
+    </section>
+  )
+}
+
+export default About
