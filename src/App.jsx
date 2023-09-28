2d584b4c0f41f9a096813c54c231e635df1ba14c
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-27 22:51:11 -0700

    add About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './Header.jsx'
 import Home from './Home.jsx'
+import About from './About.jsx'
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <About />
     </>
   )
 }
