3cb7cf886128d96fb1da600896312fce3cf2fb88
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-27 23:18:16 -0700

    change hierarchy of h tags and add Services title at top of component

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,40 +1,41 @@
 function Services() {
   return (
     <section>
+      <h2>Services</h2>
       <div>
-        <h2>Website Builds</h2>
+        <h3>Website Builds</h3>
         <div>
-          <h3>Basic Static Sites</h3>
+          <h4>Basic Static Sites</h4>
           <p>Informational site including maximum of 5 pages</p>
         </div>
         <div>
-          <h3>Advanced Static Sites</h3>
+          <h4>Advanced Static Sites</h4>
           <p>Informational site including more than 5 pages</p>
         </div>
         <div>
-          <h3>Dynamic Sites</h3>
+          <h4>Dynamic Sites</h4>
           <p>Site that requires interactive elements such as an ecommerce store, chat/comment features, a blog, or the ability to log in and present different information to each user.</p>
         </div>
       </div>
       <div>
-        <h2>Site Maintenance</h2>
+        <h3>Site Maintenance</h3>
         <div>
-          <h3>Basic Maintenance Plan</h3>
+          <h4>Basic Maintenance Plan</h4>
           <p>Monthly security updates, backups, and two simple updates to content or copy.</p>
         </div>
         <div>
-          <h3>Advanced Maintenance Plan</h3>
+          <h4>Advanced Maintenance Plan</h4>
           <p>Monthly security updates, or more frequently if necessary. Weekly backups. Unlimited updates to content or copy.</p>
         </div>
       </div>
       <div>
-        <h2>Writing</h2>
+        <h3>Writing</h3>
         <div>
-          <h3>Copy Writing</h3>
+          <h4>Copy Writing</h4>
           <p>We write the words you read on the site that convert your prospective consumers.</p>
         </div>
         <div>
-          <h3>Content Writing</h3>
+          <h4>Content Writing</h4>
           <p>Want your site to have a blog, but don&apos;t have time to write one yourself? Look no further. Monthly blog posts, you come up with the topics or approve the ones we propose.</p>
         </div>
       </div>
