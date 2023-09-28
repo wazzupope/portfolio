21cb815b170d490a2c16c004f0db4f6ee92bbd39
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-27 23:21:17 -0700

    add Portfolio and Contact components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './Header.jsx'
 import Home from './Home.jsx'
 import About from './About.jsx'
 import Services from './Services.jsx'
+import Portfolio from './Portfolio.jsx'
+import Contact from './Contact.jsx'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Portfolio />
+      <Contact />
     </>
   )
 }
