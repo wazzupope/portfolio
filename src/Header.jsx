bc43902b4a8a553bd98ee5e44b5501bf09b80556
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-27 22:16:24 -0700

    remove public from img src, per vite instructions in terminal when given command npm run dev

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import Nav from './Nav.jsx'
 function Header() {
   return (
     <header>
-      <img src='../public/assets/Polaris-black.jpg' alt='Polaris Logo'/>
+      <img src='../assets/Polaris-black.jpg' alt='Polaris Logo'/>
       <Nav />
     </header>
   )
