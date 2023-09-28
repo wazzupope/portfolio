695e1edf34b6a460c4217398bf304a6da3a67e16
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-27 23:15:07 -0700

    Add initial layout and copy

---
+++ src/Services.jsx
@@ -0,0 +1,45 @@
+function Services() {
+  return (
+    <section>
+      <div>
+        <h2>Website Builds</h2>
+        <div>
+          <h3>Basic Static Sites</h3>
+          <p>Informational site including maximum of 5 pages</p>
+        </div>
+        <div>
+          <h3>Advanced Static Sites</h3>
+          <p>Informational site including more than 5 pages</p>
+        </div>
+        <div>
+          <h3>Dynamic Sites</h3>
+          <p>Site that requires interactive elements such as an ecommerce store, chat/comment features, a blog, or the ability to log in and present different information to each user.</p>
+        </div>
+      </div>
+      <div>
+        <h2>Site Maintenance</h2>
+        <div>
+          <h3>Basic Maintenance Plan</h3>
+          <p>Monthly security updates, backups, and two simple updates to content or copy.</p>
+        </div>
+        <div>
+          <h3>Advanced Maintenance Plan</h3>
+          <p>Monthly security updates, or more frequently if necessary. Weekly backups. Unlimited updates to content or copy.</p>
+        </div>
+      </div>
+      <div>
+        <h2>Writing</h2>
+        <div>
+          <h3>Copy Writing</h3>
+          <p>We write the words you read on the site that convert your prospective consumers.</p>
+        </div>
+        <div>
+          <h3>Content Writing</h3>
+          <p>Want your site to have a blog, but don&apos;t have time to write one yourself? Look no further. Monthly blog posts, you come up with the topics or approve the ones we propose.</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Services
