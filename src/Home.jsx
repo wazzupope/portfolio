1060bf403f8a97a373d089fb446045a3addc38df
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-27 22:35:16 -0700

    add initial copy to component

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main>
+      <h1>Specializing in health, wellness, and athletics websites</h1>
+      <h2>Polaris guides you to a web presence that stands out in your community</h2>
+      <h3>Sites optimized to attract your ideal consumers</h3>
+    </main>
+  )
+}
+
+export default Home
