30dd90bbca06922bd31b2a2f265206e5bee16ec5
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-29 23:25:33 -0700

    import css and create component for hamburger menu

---
+++ src/Hamburger.jsx
@@ -0,0 +1,12 @@
+import './Hamburger.css'
+
+function Hamburger() {
+  return (
+    <div className="hamburger">
+      <span className="hamburger-bun-top"></span>
+      <span className="hamburger-bun-bottom"></span>
+    </div>
+  )
+}
+
+export default Hamburger
