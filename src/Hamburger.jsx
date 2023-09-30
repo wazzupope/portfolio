4469beb7089f225824580cd66623461280b34c52
jpfairhart <johnpaulfairhart@hotmail.com>
2023-09-29 23:38:48 -0700

    add function to handle click

--- src/Hamburger.jsx
+++ src/Hamburger.jsx
@@ -1,8 +1,14 @@
 import './Hamburger.css'
 
 function Hamburger() {
+  let burger = document.getElementByClassName('hamburger')
+  
+  function handleClick() {
+    burger.toggleClass('open')
+  }
+
   return (
-    <div className="hamburger">
+    <div className="hamburger" onClick={handleClick}>
       <span className="hamburger-bun-top"></span>
       <span className="hamburger-bun-bottom"></span>
     </div>
