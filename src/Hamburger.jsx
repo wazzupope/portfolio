b2a0d1054dad41baa9eb3647488e5e30cfb75289
jpfairhart <johnpaulfairhart@hotmail.com>
2023-10-01 23:22:45 -0700

    add logic for changing to/from active class for hamburger menu, need to debug why not working

--- src/Hamburger.jsx
+++ src/Hamburger.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import './Hamburger.css'
 
 function Hamburger() {
-  const [isActive, setActive] = useState("false");
+  const [isActive, setActive] = useState(false);
 
-  const ToggleClass = () => {
+  const handleToggle = () => {
     setActive(!isActive);
   };
 
   return (
-    <div className="hamburger {isActive ? active : inactive}" onClick={ToggleClass}>
+    <div className="hamburger (isActive ? active : null)" onClick={handleToggle}>
       <span className="hamburger-bun-top"></span>
       <span className="hamburger-bun-bottom"></span>
     </div>
