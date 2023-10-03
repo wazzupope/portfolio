c9541dd479396594abfea36ea7710651045915e8
jpfairhart <johnpaulfairhart@hotmail.com>
2023-10-02 23:44:27 -0700

    pass state to Hamburger as props for opening/closing nav hamburger menu, needs debugging

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,19 +1,41 @@
+import { useState } from 'react'
 import './Nav.css'
 import Hamburger from './Hamburger.jsx'
 
+{/*
+Need to debug why passing state as props to Hamburger isn't working properly
+Can remove position: absolute in browser devtools to be able to see hamburger element
+*/}
+
+
 function Nav() {
-  return (
-    <nav>
-      <Hamburger />
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  )
+  const [isActive, setActive] = useState(null);
+
+  const handleToggle = () => {
+    setActive(isActive ? null : "active");
+  };
+
+  if (isActive) {  
+    return (
+      <nav>
+        <Hamburger isOpen='isActive' onClick={handleToggle} />
+        <ul className="nav-list">
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    )
+  }
+  else {
+    return (
+      <nav>
+        <Hamburger isOpen='isActive' onClick={handleToggle}/>
+      </nav>
+    )
+  }
 }
 
 export default Nav
