7f864f83b265e6be6c2b22b047b50ff66f12d96a
jpfairhart <johnpaulfairhart@hotmail.com>
2023-10-02 23:41:33 -0700

    modify to use props from Nav to modify state, needs debugging

--- src/Hamburger.jsx
+++ src/Hamburger.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
 import './Hamburger.css'
 
-function Hamburger() {
-  const [isActive, setActive] = useState(null);
-
-  const handleToggle = () => {
-    setActive(isActive ? null : "active");
-  };
+function Hamburger({ isOpen }) {
+  let burgerOpen = isOpen;
 
   return (
-    <div className={isActive ? "hamburger active" : "hamburger"} onClick={handleToggle}>
+    <div className={burgerOpen ? "hamburger active" : "hamburger"}>
       <span className="hamburger-bun-top"></span>
       <span className="hamburger-bun-bottom"></span>
     </div>
