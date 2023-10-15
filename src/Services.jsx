34fc911ce9fd26f4bd4802d961e514c7b3806e66
jpfairhart <johnpaulfairhart@hotmail.com>
2023-10-14 22:59:20 -0700

    edit text content

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,44 +1,14 @@
 function Services() {
   return (
     <section>
-      <h2>Services</h2>
-      <div>
-        <h3>Website Builds</h3>
-        <div>
-          <h4>Basic Static Sites</h4>
-          <p>Informational site including maximum of 5 pages</p>
-        </div>
-        <div>
-          <h4>Advanced Static Sites</h4>
-          <p>Informational site including more than 5 pages</p>
-        </div>
-        <div>
-          <h4>Dynamic Sites</h4>
-          <p>Site that requires interactive elements such as an ecommerce store, chat/comment features, a blog, or the ability to log in and present different information to each user.</p>
-        </div>
-      </div>
-      <div>
-        <h3>Site Maintenance</h3>
-        <div>
-          <h4>Basic Maintenance Plan</h4>
-          <p>Monthly security updates, backups, and two simple updates to content or copy.</p>
-        </div>
-        <div>
-          <h4>Advanced Maintenance Plan</h4>
-          <p>Monthly security updates, or more frequently if necessary. Weekly backups. Unlimited updates to content or copy.</p>
-        </div>
-      </div>
-      <div>
-        <h3>Writing</h3>
-        <div>
-          <h4>Copy Writing</h4>
-          <p>We write the words you read on the site that convert your prospective consumers.</p>
-        </div>
-        <div>
-          <h4>Content Writing</h4>
-          <p>Want your site to have a blog, but don&apos;t have time to write one yourself? Look no further. Monthly blog posts, you come up with the topics or approve the ones we propose.</p>
-        </div>
-      </div>
+      <h2>Skills/Technologies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Firebase</li>
+      </ul>
     </section>
   )
 }
