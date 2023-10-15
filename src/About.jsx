459c129cc993aa02e5e9c006bf95551c2e09b45a
jpfairhart <johnpaulfairhart@hotmail.com>
2023-10-14 22:55:52 -0700

    edit text content

--- src/About.jsx
+++ src/About.jsx
@@ -3,17 +3,17 @@ function About() {
     <section>
       <div>
         <h2>About</h2>
-        <p>Founded by a physical therapist, Polaris is the authority on fast websites for medical clinics, wellness practitioners, and sporting organizations. Here&apos;s what sets Polaris apart:</p>
+        <p>Originally a physical therapist by trade, my skillset is unique to the web development field.</p>
       </div>
       <div>
         <h3>Communication</h3>
-        <p>You will never be left in the dark about the status of your project. Reach out via your preferred method, hear back within 24 hours.</p>
+        <p>A decade interacting with people of all walks of life with their health at stake teaches to you to be an effective communicator.</p>
       </div>
-        <h3>Control</h3>
-        <p>Choose how many revisions to your project you would like. Want complete control over design, layout, copy writing, all of it? Don&apos;t want to be bothered until it&apos;s done? Whatever you prefer, you got it.</p>
+        <h3>Attention to Detail</h3>
+        <p>Following insurance guidelines, making note of every detail of treatments and being able to justify it in written form. This skill has led to my ability to document my code in a clear and concise manner.</p>
       <div>
-        <h3>Conversion</h3>
-        <p>Our sites are optimized for speed and search engine performance. Care is also put into writing the copy for the site to ensure that your target audience gives you the attention you deserve.</p>
+        <h3>Leadership</h3>
+        <p>Experience in leadership roles throughout my career has given me perspective on how to be a good teammate and employee.</p>
       </div>
     </section>
   )
