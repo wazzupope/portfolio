a172df3708b8e28d1efcb1f9c8b2bd927ba03ba0
jpfairhart <johnpaulfairhart@hotmail.com>
2023-10-14 23:04:22 -0700

    renamed Services.jsx to Skills.jsx

---
+++ src/Skills.jsx
@@ -0,0 +1,16 @@
+function Services() {
+  return (
+    <section>
+      <h2>Skills/Technologies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Firebase</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Services
