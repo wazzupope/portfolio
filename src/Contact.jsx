a7dc97b257cd07b87b478fb1bdff4984fc99af2d
jpfairhart <johnpaulfairhart@hotmail.com>
2023-11-01 10:09:19 -0700

    add links to various methods of contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,6 +2,9 @@ function Contact() {
   return (
     <section>
       <h2>Contact</h2>
+      <a href="">Email</a>
+      <a href="">LinkedIn</a>
+      <a href="">GitHub</a>
     </section>
   )
 }
