215df987744cc7c87a145669bf08a59aca1709ae
jpfairhart <johnpaulfairhart@hotmail.com>
2023-11-01 10:00:13 -0700

    add more skill li elements

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -5,9 +5,13 @@ function Skills() {
       <ul>
         <li>HTML</li>
         <li>CSS</li>
+        <li>Tailwind CSS</li>
         <li>Javascript</li>
-        <li>React</li>
+        <li>ReactJS</li>
+        <li>NextJS</li>
         <li>Firebase</li>
+        <li>Git</li>
+        <li>GitHub</li>
       </ul>
     </section>
   )
