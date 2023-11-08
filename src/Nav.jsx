07b981ed6284b03888358a6007dc216e51a7def4
jpfairhart <johnpaulfairhart@hotmail.com>
2023-11-07 15:32:11 -1000

    use react-burger-menu to create initial hamburger menu with routing, not yet styled

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,9 +1,16 @@
+import { slide as Menu } from 'react-burger-menu'
 import './Nav.css'
 
 function Nav() {
   return (
     <nav>
-
+      <Menu right>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="skills" className="menu-item" href="/skills">Skills</a>
+        <a id="portfolio" className="menu-item" href="/portfolio">Portfolio</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+      </Menu>
     </nav>
   )
 }
