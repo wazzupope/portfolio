825cdd09b4591f0392ddef573b67ded9229c1b00
jpfairhart <johnpaulfairhart@hotmail.com>
2023-11-07 15:46:36 -1000

    add initial basic formatting and image of screenshot of first project

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,7 +1,14 @@
+// wrap h3 and img with link tags that go to https://lfizzle-invitational.web.app/
+
 function Portfolio() {
   return (
     <section>
       <h2>Portfolio</h2>
+      <div>
+        <h3>LFizzle Invitational</h3>
+        <p>Golf tournament website created using React with live scoring feature.</p>
+        <img src='/assets/LFI-screenshot.png' alt='Thumbnail of golf tournament website'></img>
+      </div>
     </section>
   )
 }
