066390e2f8b006f249d81e84ed0e5a30cbb416df
jpfairhart <johnpaulfairhart@hotmail.com>
2023-11-07 15:08:23 -1000

    remove attempt at hamburger menu

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,41 +1,11 @@
-import { useState } from 'react'
 import './Nav.css'
-import Hamburger from './Hamburger.jsx'
-
-{/*
-Need to debug why passing state as props to Hamburger isn't working properly
-Can remove position: absolute in browser devtools to be able to see hamburger element
-*/}
-
 
 function Nav() {
-  const [isActive, setActive] = useState(null);
+  return (
+    <nav>
 
-  const handleToggle = () => {
-    setActive(isActive ? null : "active");
-  };
-
-  if (isActive) {  
-    return (
-      <nav>
-        <Hamburger isOpen='isActive' onClick={handleToggle} />
-        <ul className="nav-list">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    )
-  }
-  else {
-    return (
-      <nav>
-        <Hamburger isOpen='isActive' onClick={handleToggle}/>
-      </nav>
-    )
-  }
+    </nav>
+  )
 }
 
 export default Nav
