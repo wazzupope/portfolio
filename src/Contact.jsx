168d77fba3e20956c729ae0a658e07c70a05513d
jpfairhart <johnpaulfairhart@hotmail.com>
2024-03-15 00:06:29 -0700

    use logos/icons as links

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,9 +4,9 @@ function Contact() {
   return (
     <section>
       <h2>Contact</h2>
-      <a href="mailto:johnpaulfairhart@hotmail.com">Email</a>
-      <a href="https://www.linkedin.com/in/jp-fairhart">LinkedIn</a>
-      <a href="https://github.com/wazzupope">GitHub</a>
+      <a href="mailto:johnpaulfairhart@hotmail.com"><img src="../public/assets/email-svg-white-lines.svg" alt="email icon"/></a>
+      <a href="https://www.linkedin.com/in/jp-fairhart"><img src="../public/assets/linkedin-svg-white-letters.svg" alt="linkedin logo"/></a>
+      <a href="https://github.com/wazzupope"><img src="../public/assets/github-mark-white.svg" alt="github logo"/></a>
     </section>
   )
 }
