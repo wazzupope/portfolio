2c831fbaa08144f00770a25b76750d956ee95929
jpfairhart <johnpaulfairhart@hotmail.com>
2024-03-15 23:49:35 -0700

    add id to main element

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <main>
+    <main id="hero">
       <h1>Fairhart Web Development</h1>
       <h2>Specializing in health, wellness, and athletics websites</h2>
     </main>
