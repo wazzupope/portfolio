775a20d84f7e833f355aac17f3fa47b6476b4568
jpfairhart <johnpaulfairhart@hotmail.com>
2024-03-15 23:43:43 -0700

    add id to header element

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import Nav from './Nav.jsx'
 
 function Header() {
   return (
-    <header>
+    <header id='header'>
       <img id='white-FWD-logo' src='/assets/FWD-logo-white.jpg' alt='Fairhart Web Development Logo'/>
       <Nav />
     </header>
