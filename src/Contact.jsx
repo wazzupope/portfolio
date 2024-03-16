c0b99f3a56ff0a6e9a604443632f83edd9fc165e
jpfairhart <johnpaulfairhart@hotmail.com>
2024-03-15 23:26:55 -0700

    add className to img tags

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,11 +3,10 @@ import './Contact.css'
 function Contact() {
   return (
     <section id='contact-section'>
-      <h2>Contact</h2>
       <div id='logo-box'>
-        <a href="mailto:johnpaulfairhart@hotmail.com"><img src="../public/assets/email-svg-white-lines.svg" alt="email icon"/></a>
-        <a href="https://www.linkedin.com/in/jp-fairhart"><img src="../public/assets/linkedin-svg-white-letters.svg" alt="linkedin logo"/></a>
-        <a href="https://github.com/wazzupope"><img src="../public/assets/github-mark-white.svg" alt="github logo"/></a>
+        <a href="mailto:johnpaulfairhart@hotmail.com"><img className='contact-links' src="../public/assets/email-svg-white-lines.svg" alt="email icon"/></a>
+        <a href="https://www.linkedin.com/in/jp-fairhart"><img className='contact-links' src="../public/assets/linkedin-svg-white-letters.svg" alt="linkedin logo"/></a>
+        <a href="https://github.com/wazzupope"><img className='contact-links' src="../public/assets/github-mark-white.svg" alt="github logo"/></a>
       </div>
     </section>
   )
