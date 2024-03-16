995337968188c6e2c2b44eb7cf9df4caaa2bf713
jpfairhart <johnpaulfairhart@hotmail.com>
2024-03-15 23:05:55 -0700

    import css file, put div around logo links

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,14 @@
-// How to import Contact.css styles? With <style> tag?
+import './Contact.css'
 
 function Contact() {
   return (
     <section>
       <h2>Contact</h2>
-      <a href="mailto:johnpaulfairhart@hotmail.com"><img src="../public/assets/email-svg-white-lines.svg" alt="email icon"/></a>
-      <a href="https://www.linkedin.com/in/jp-fairhart"><img src="../public/assets/linkedin-svg-white-letters.svg" alt="linkedin logo"/></a>
-      <a href="https://github.com/wazzupope"><img src="../public/assets/github-mark-white.svg" alt="github logo"/></a>
+      <div id='logo-box'>
+        <a href="mailto:johnpaulfairhart@hotmail.com"><img src="../public/assets/email-svg-white-lines.svg" alt="email icon"/></a>
+        <a href="https://www.linkedin.com/in/jp-fairhart"><img src="../public/assets/linkedin-svg-white-letters.svg" alt="linkedin logo"/></a>
+        <a href="https://github.com/wazzupope"><img src="../public/assets/github-mark-white.svg" alt="github logo"/></a>
+      </div>
     </section>
   )
 }
