7e0e14a8cbafd682b28f1b4906621a10e352ffae
jpfairhart <johnpaulfairhart@hotmail.com>
2024-03-26 00:05:40 -0700

    add ids and classes to elements

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,17 +1,17 @@
 function Skills() {
   return (
     <section id='skills-section'>
-      <h2>Skills/Technologies</h2>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Tailwind CSS</li>
-        <li>Javascript</li>
-        <li>ReactJS</li>
-        <li>NextJS</li>
-        <li>Firebase</li>
-        <li>Git</li>
-        <li>GitHub</li>
+      <h2 id='skills-header'>Skills/Technologies</h2>
+      <ul id='skills-list'>
+        <li className='skills-list-item'>HTML</li>
+        <li className='skills-list-item'>CSS</li>
+        <li className='skills-list-item'>Tailwind CSS</li>
+        <li className='skills-list-item'>Javascript</li>
+        <li className='skills-list-item'>ReactJS</li>
+        <li className='skills-list-item'>NextJS</li>
+        <li className='skills-list-item'>Firebase</li>
+        <li className='skills-list-item'>Git</li>
+        <li className='skills-list-item'>GitHub</li>
       </ul>
     </section>
   )
