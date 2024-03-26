c28aadbc66abe8e621032b46f4ff54b1420993e8
jpfairhart <johnpaulfairhart@hotmail.com>
2024-03-25 23:54:01 -0700

    update links to internal element ids

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,11 +5,11 @@ function Nav() {
   return (
     <nav id='nav'>
       <Menu right>
-        <a id="home" className="menu-item" href="/">HOME</a>
-        <a id="about" className="menu-item" href="/about">ABOUT</a>
-        <a id="skills" className="menu-item" href="/skills">SKILLS</a>
-        <a id="portfolio" className="menu-item" href="/portfolio">PORTFOLIO</a>
-        <a id="contact" className="menu-item" href="/contact">CONTACT</a>
+        <a id="home" className="menu-item" href="#header">HOME</a>
+        <a id="about" className="menu-item" href="#about-section">ABOUT</a>
+        <a id="skills" className="menu-item" href="#skills-section">SKILLS</a>
+        <a id="portfolio" className="menu-item" href="#portfolio-section">PORTFOLIO</a>
+        <a id="contact" className="menu-item" href="#contact-section">CONTACT</a>
       </Menu>
     </nav>
   )
