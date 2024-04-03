578ce9909d3023d7e7909bc371560242259a55aa
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-02 23:23:29 -0700

    remove public folder from filepaths for images

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,9 +4,9 @@ function Contact() {
   return (
     <section id='contact-section'>
       <div id='logo-box'>
-        <a href="mailto:johnpaulfairhart@hotmail.com"><img className='contact-links' src="../public/assets/email-svg-white-lines.svg" alt="email icon"/></a>
-        <a href="https://www.linkedin.com/in/jp-fairhart"><img className='contact-links' src="../public/assets/linkedin-svg-white-letters.svg" alt="linkedin logo"/></a>
-        <a href="https://github.com/wazzupope"><img className='contact-links' src="../public/assets/github-mark-white.svg" alt="github logo"/></a>
+        <a href="mailto:johnpaulfairhart@hotmail.com"><img className='contact-links' src="../assets/email-svg-white-lines.svg" alt="email icon"/></a>
+        <a href="https://www.linkedin.com/in/jp-fairhart"><img className='contact-links' src="../assets/linkedin-svg-white-letters.svg" alt="linkedin logo"/></a>
+        <a href="https://github.com/wazzupope"><img className='contact-links' src="../assets/github-mark-white.svg" alt="github logo"/></a>
       </div>
     </section>
   )
