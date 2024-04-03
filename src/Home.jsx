40bf3c9ebe1b1e8f146a62c9287600b390232e7f
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-02 23:49:47 -0700

    add setInterval function from 'apple tv' component, unedited

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,19 @@
 function Home() {
+  setInterval(function () {
+    const show = document.querySelector('span[data-show]')
+    const next = show.nextElementSibling || document.querySelector('span:first-child')
+    const up = document.querySelector('span[data-up]')
+    
+    if (up) {
+      up.removeAttribute('data-up')
+    }
+    
+    show.removeAttribute('data-show')
+    show.setAttribute('data-up', '')
+    
+    next.setAttribute('data-show', '')
+  }, 2000)
+  
   return (
     <main id="hero">
       <h1>Fairhart Web Development</h1>
