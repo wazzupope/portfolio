bbdee2ef1c5a28201c7a31cb3a9c841ab3ed0e86
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-04 23:46:00 -0700

    change ids of links to avoid conflict with hrefs

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,11 +5,11 @@ function Nav() {
   return (
     <nav id='nav'>
       <Menu right>
-        <a id="home" className="menu-item" href="#header">HOME</a>
-        <a id="about" className="menu-item" href="#about">ABOUT</a>
-        <a id="skills" className="menu-item" href="#skills">SKILLS</a>
-        <a id="portfolio" className="menu-item" href="#portfolio">PORTFOLIO</a>
-        <a id="contact" className="menu-item" href="#contact">CONTACT</a>
+        <a id="home-link" className="menu-item" href="#header">HOME</a>
+        <a id="about-link" className="menu-item" href="#about">ABOUT</a>
+        <a id="skills-link" className="menu-item" href="#skills">SKILLS</a>
+        <a id="portfolio-link" className="menu-item" href="#portfolio">PORTFOLIO</a>
+        <a id="contact-link" className="menu-item" href="#contact">CONTACT</a>
       </Menu>
     </nav>
   )
