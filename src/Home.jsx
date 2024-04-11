3665ae7d60c97e2f184cfe61835e93cae59d0961
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-10 23:41:28 -0700

    import css module, add ids to jsx elements

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,13 @@
 import Animation from './TypeAnimation.jsx'
+import './Home.css'
 
 function Home() {
 
   return (
     <main id="hero">
-      <h1>Fairhart Web Development</h1>
-      <h2>
-        Your web solution for <Animation />
+      <h1 id='hero-header'>Fairhart Web Development</h1>
+      <h2 id='hero-subheader'>
+        Your web solution for <Animation id='animation' />
       </h2>
     </main>
   )
