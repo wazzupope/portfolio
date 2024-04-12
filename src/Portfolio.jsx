1969dc11b9b9b7dd2aeff7cfd72cab726a927335
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-11 22:34:27 -0700

    add ids and classnames

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -3,12 +3,12 @@ import './Portfolio.css'
 function Portfolio() {
   return (
     <section id='portfolio'>
-      <h2>Portfolio</h2>
+      <h2 id='portfolio-header'>Portfolio</h2>
       <div>
-        <h3>LFizzle Invitational</h3>
-        <p>Golf tournament website created using React with live scoring feature.</p>
-        <a id='LFI-link' href="https://lfizzle-invitational.web.app">
-          <img id='LFI-screenshot' src='/assets/LFI-screenshot.png' alt='Thumbnail of golf tournament website'></img>
+        <h3 className='portfolio-subheader'>LFizzle Invitational</h3>
+        <p className='portfolio-description'>Golf tournament website created using React with live scoring feature.</p>
+        <a id='LFI-link' className='portfolio-link' href="https://lfizzle-invitational.web.app">
+          <img id='LFI-screenshot' className='portfolio-screenshot' src='/assets/LFI-screenshot.png' alt='Thumbnail of golf tournament website'></img>
         </a>
       </div>
     </section>
