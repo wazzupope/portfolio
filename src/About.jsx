a82d179d1f6f73f102d5498a3759a63bb6b7ba6c
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-12 11:09:59 -0700

    add classnames to divs within section element for styling

--- src/About.jsx
+++ src/About.jsx
@@ -3,17 +3,17 @@ import './About.css'
 function About() {
   return (
     <section id='about'>
-      <div>
+      <div className='about-content-div'>
         <h2>About</h2>
         <p>Originally a physical therapist by trade, my skillset is unique to the web development field.</p>
       </div>
-      <div>
+      <div className='about-content-div'>
         <h3>Communication</h3>
         <p>A decade interacting with people of all walks of life with their health at stake teaches to you to be an effective communicator.</p>
       </div>
         <h3>Attention to Detail</h3>
         <p>Following insurance guidelines, making note of every detail of treatments and being able to justify it in written form. This skill has led to my ability to document my code in a clear and concise manner.</p>
-      <div>
+      <div className='about-content-div'>
         <h3>Leadership</h3>
         <p>Experience in leadership roles throughout my career has given me perspective on how to be a good teammate and employee.</p>
       </div>
