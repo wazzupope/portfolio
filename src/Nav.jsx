d0e619f2b139660eed08ef0d52c7575590c9daaa
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-12 23:34:33 -0700

    add handling for closing sidebar nav when a link is clicked or a click occurs outside the menu

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,33 @@
+import { useState, useRef } from 'react'
 import { slide as Menu } from 'react-burger-menu'
 import './Nav.css'
 
 function Nav() {
+  // handlers for closing nav when a link within the nav is clicked or when there is a click outside the menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const handleMenuOpen = () => setIsMenuOpen(!isMenuOpen)
+  const closeMenu = () => setIsMenuOpen(false)
+  const navMenu = useRef(null)
+  const closeOpenMenu = (e) => {
+    if (navMenu.current && !navMenu.current.contains(e.target)) {
+      setIsMenuOpen(false)
+    }
+  }
+  document.addEventListener('mousedown', closeOpenMenu)
+
   return (
-    <nav id='nav'>
-      <Menu right>
+    <nav id='nav' ref={navMenu}>
+      <Menu
+      right
+      isOpen={isMenuOpen}
+      onOpen={handleMenuOpen}
+      onClose={handleMenuOpen}
+      >
         <a id="home-link" className="menu-item" href="/">HOME</a>
-        <a id="portfolio-link" className="menu-item" href="#portfolio">PORTFOLIO</a>
-        <a id="about-link" className="menu-item" href="#about">ABOUT</a>
-        <a id="skills-link" className="menu-item" href="#skills">SKILLS</a>
-        <a id="contact-link" className="menu-item" href="#contact">CONTACT</a>
+        <a onClick={closeMenu} id="portfolio-link" className="menu-item" href="#portfolio">PORTFOLIO</a>
+        <a onClick={closeMenu} id="about-link" className="menu-item" href="#about">ABOUT</a>
+        <a onClick={closeMenu} id="skills-link" className="menu-item" href="#skills">SKILLS</a>
+        <a onClick={closeMenu} id="contact-link" className="menu-item" href="#contact">CONTACT</a>
       </Menu>
     </nav>
   )
