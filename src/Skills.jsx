dcecd34b6a11e5f9967be7834258ab64b85ede93
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-14 23:27:17 -0700

    change header text

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -3,7 +3,7 @@ import './Skills.css'
 function Skills() {
   return (
     <section id='skills'>
-      <h2 id='skills-header'>Skills/Technologies</h2>
+      <h2 id='skills-header'>Skills</h2>
       <ul id='skills-list'>
         <li className='skills-list-item'>HTML</li>
         <li className='skills-list-item'>CSS</li>
