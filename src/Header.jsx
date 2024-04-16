5b549c017c3b4df00bbf762e8dc96750e8f28f45
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-15 23:16:20 -0700

    add sidebar container for display on desktop screen widths

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,6 +5,14 @@ function Header() {
   return (
     <header id='header'>
       <a href='/'><img id='white-FWD-logo' src='/assets/FWD-logo-white.jpg' alt='Fairhart Web Development Logo'/></a>
+      {/* menu for screen sizes 768px or greater */}
+      <div id='sidebar-container'>
+        <a id="home-link" className="menu-item" href="/">HOME</a>
+        <a id="portfolio-link" className="menu-item" href="#portfolio">PORTFOLIO</a>
+        <a id="about-link" className="menu-item" href="#about">ABOUT</a>
+        <a id="skills-link" className="menu-item" href="#skills">SKILLS</a>
+        <a id="contact-link" className="menu-item" href="#contact">CONTACT</a>
+      </div>
       <Nav />
     </header>
   )
