43da8492b208e19aca4e8ae12df05d665b8be2b8
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-24 22:48:29 -0700

    reorganize layout into two separate lists with containers

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -5,15 +5,25 @@ function Skills() {
     <section id='skills'>
       <h2 id='skills-header'>Skills</h2>
       <div id='skills-container'>
-        <li className='skills-list-item'>HTML</li>
-        <li className='skills-list-item'>CSS</li>
-        <li className='skills-list-item'>Javascript</li>
-        <li className='skills-list-item'>ReactJS</li>
-        <li className='skills-list-item'>Git</li>
-        <li className='skills-list-item'>GitHub</li>
-        <li className='skills-list-item'>NextJS</li>
-        <li className='skills-list-item'>Tailwind CSS</li>
-        <li className='skills-list-item'>Firebase</li>
+        <div id='stronger-skills-container'>
+          <h3 className='skills-subheader'>Most Used Technologies</h3>
+          <ul id='stronger-skills-list' className='skills-list'>
+            <li className='skills-list-item'>HTML</li>
+            <li className='skills-list-item'>CSS</li>
+            <li className='skills-list-item'>Javascript</li>
+            <li className='skills-list-item'>ReactJS</li>
+            <li className='skills-list-item'>Git</li>
+            <li className='skills-list-item'>GitHub</li>
+          </ul>
+        </div>
+        <div id='other-skills-container'>
+          <h3 className='skills-subheader'>Other Technologies</h3>
+          <ul id='other-skills-list' className='skills-list'>
+            <li className='skills-list-item'>NextJS</li>
+            <li className='skills-list-item'>Tailwind CSS</li>
+            <li className='skills-list-item'>Firebase</li>
+          </ul>
+        </div>
       </div>
     </section>
   )
