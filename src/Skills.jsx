de91f854c78a01b946b2430e167a845ff0234b02
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-24 22:39:02 -0700

    change ul to div and change id to skills-container, reorder list items

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -4,17 +4,17 @@ function Skills() {
   return (
     <section id='skills'>
       <h2 id='skills-header'>Skills</h2>
-      <ul id='skills-list'>
+      <div id='skills-container'>
         <li className='skills-list-item'>HTML</li>
         <li className='skills-list-item'>CSS</li>
-        <li className='skills-list-item'>Tailwind CSS</li>
         <li className='skills-list-item'>Javascript</li>
         <li className='skills-list-item'>ReactJS</li>
-        <li className='skills-list-item'>NextJS</li>
-        <li className='skills-list-item'>Firebase</li>
         <li className='skills-list-item'>Git</li>
         <li className='skills-list-item'>GitHub</li>
-      </ul>
+        <li className='skills-list-item'>NextJS</li>
+        <li className='skills-list-item'>Tailwind CSS</li>
+        <li className='skills-list-item'>Firebase</li>
+      </div>
     </section>
   )
 }
