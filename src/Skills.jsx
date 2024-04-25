78d3327929af5880f5d3beb1597ebfed81542044
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-24 23:28:15 -0700

    add technology logos and reorganize list items to contain image and span

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -8,20 +8,71 @@ function Skills() {
         <div id='stronger-skills-container'>
           <h3 className='skills-subheader'>Most Used Technologies</h3>
           <ul id='stronger-skills-list' className='skills-list'>
-            <li className='skills-list-item'>HTML</li>
-            <li className='skills-list-item'>CSS</li>
-            <li className='skills-list-item'>Javascript</li>
-            <li className='skills-list-item'>ReactJS</li>
-            <li className='skills-list-item'>Git</li>
-            <li className='skills-list-item'>GitHub</li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/html5-logo.svg" alt="HTML 5 icon"/>
+              <span>
+                HTML
+              </span>
+            </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/css3-logo.svg" alt="CSS 3 icon"/>
+              <span>
+                CSS
+              </span>
+            </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/javascript-logo.svg" alt="Javascript icon"/>
+              <span>
+                JavaScript
+              </span>
+            </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/react-logo.svg" alt="React icon"/>
+              <span>
+                React
+              </span>
+            </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/git-logo.svg" alt="Git icon"/>
+              <span>
+                Git
+              </span>
+            </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/github-mark-white.svg" alt="GitHub icon"/>
+              <span>
+                GitHub
+              </span>
+            </li>
           </ul>
         </div>
         <div id='other-skills-container'>
           <h3 className='skills-subheader'>Other Technologies</h3>
           <ul id='other-skills-list' className='skills-list'>
-            <li className='skills-list-item'>NextJS</li>
-            <li className='skills-list-item'>Tailwind CSS</li>
-            <li className='skills-list-item'>Firebase</li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/nextjs-logo.svg" alt="Next.js icon"/>
+              <span>
+                Next.js
+              </span>
+            </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/node-logo.svg" alt="Node.js icon"/>
+              <span>
+                Node.js
+              </span>
+            </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/tailwind-logo.svg" alt="Tailwind CSS icon"/>
+              <span>
+                Tailwind CSS
+              </span>
+              </li>
+            <li className='skills-list-item'>
+              <img className='skill-logo-image' src="../assets/firebase-logo.svg" alt="Firebase icon"/>
+              <span>
+                Firebase
+              </span>
+            </li>
           </ul>
         </div>
       </div>
