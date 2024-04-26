f5462d9f9680231e235ebbd29e22d9e52d7cffe6
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-25 22:14:26 -0700

    add class to spans with text for names of skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -10,37 +10,37 @@ function Skills() {
           <ul id='stronger-skills-list' className='skills-list'>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/html5-logo.svg" alt="HTML 5 icon"/>
-              <span>
+              <span className='skill-name'>
                 HTML
               </span>
             </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/css3-logo.svg" alt="CSS 3 icon"/>
-              <span>
+              <span className='skill-name'>
                 CSS
               </span>
             </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/javascript-logo.svg" alt="Javascript icon"/>
-              <span>
+              <span className='skill-name'>
                 JavaScript
               </span>
             </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/react-logo.svg" alt="React icon"/>
-              <span>
+              <span className='skill-name'>
                 React
               </span>
             </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/git-logo.svg" alt="Git icon"/>
-              <span>
+              <span className='skill-name'>
                 Git
               </span>
             </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/github-mark-white.svg" alt="GitHub icon"/>
-              <span>
+              <span className='skill-name'>
                 GitHub
               </span>
             </li>
@@ -51,25 +51,25 @@ function Skills() {
           <ul id='other-skills-list' className='skills-list'>
             <li className='skills-list-item'>
               <img id='next-logo' className='skill-logo-image' src="../assets/nextjs-logo.svg" alt="Next.js icon"/>
-              <span>
+              <span className='skill-name'>
                 Next.js
               </span>
             </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/node-logo.svg" alt="Node.js icon"/>
-              <span>
+              <span className='skill-name'>
                 Node.js
               </span>
             </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/tailwind-logo.svg" alt="Tailwind CSS icon"/>
-              <span>
-                Tailwind CSS
+              <span className='skill-name'>
+                Tailwind
               </span>
               </li>
             <li className='skills-list-item'>
               <img className='skill-logo-image' src="../assets/firebase-logo.svg" alt="Firebase icon"/>
-              <span>
+              <span className='skill-name'>
                 Firebase
               </span>
             </li>
