636be27eddbc03a5b3c700c7e2d755d9681e23de
jpfairhart <johnpaulfairhart@hotmail.com>
2024-04-28 23:02:37 -0700

    add basic footer

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <section id="footer-section">
+      <img id='footer-FWD-logo' src='/assets/FWD-logo-black.jpg' alt='Fairhart Web Development Logo'/>
+      <p>© 2024 Fairhart Web Development</p>
+    </section>
+  )
+}
+
+export default Footer
